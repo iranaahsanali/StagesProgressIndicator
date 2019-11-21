d91262b945e45d1f24787da9fcb3b3a3b7c29956
Ahsan Ali <>
2019-11-21 17:49:30 +0500

    all files added

---
+++ StagesProgressIndicator/StagesProgressIndicator/StagesProgressIndicator.h
@@ -0,0 +1,19 @@
+//
+//  StagesProgressIndicator.h
+//  StagesProgressIndicator
+//
+//  Created by Ahsan Ali on 17/10/2019.
+//  Copyright © 2019 Ahsan Ali. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StagesProgressIndicator.
+FOUNDATION_EXPORT double StagesProgressIndicatorVersionNumber;
+
+//! Project version string for StagesProgressIndicator.
+FOUNDATION_EXPORT const unsigned char StagesProgressIndicatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StagesProgressIndicator/PublicHeader.h>
+
+
